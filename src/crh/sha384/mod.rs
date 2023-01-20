807473b8d9c03503f275e0b15495a9ae9cf4d097
Tom Godden <tom.godden@vub.be>
2023-01-20 09:55:25 +0100

    Fixed the comment on size of Output for sha384 and sha512

--- src/crh/sha384/mod.rs
+++ src/crh/sha384/mod.rs
@@ -40,7 +40,7 @@ impl CRHScheme for Sha384 {
 
 impl TwoToOneCRHScheme for Sha384 {
     type Input = [u8];
-    // This is always 32 bytes. It has to be a Vec to impl CanonicalSerialize
+    // This is always 48 bytes. It has to be a Vec to impl CanonicalSerialize
     type Output = Vec<u8>;
     // There are no parameters for SHA384
     type Parameters = ();
